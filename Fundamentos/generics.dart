8297ba18d502bbec8ee698f0b7574d021fb0f56d
Higor <higor_pinho@estudante.sc.senai.br>
2020-05-24 16:42:14 -0300

    009 - Estudando Generics em dart

---
+++ Fundamentos/generics.dart
@@ -0,0 +1,15 @@
+main() {
+  print('Inicio');
+  List<String> frutas = ['Banana', 'Maçã', 'Laranja'];
+  frutas.add('Melão');
+
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiario': 600.00,
+  };
+
+  print(salarios);
+}
